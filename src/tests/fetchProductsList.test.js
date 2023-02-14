1d7122b1293ea1b873121a9660312fb0921f2e72
Daniel <daniel.gomes.9@hotmail.com>
2023-02-14 15:21:49 -0300

    requisito 1 realizado

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -4,18 +4,21 @@ import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
-  it('fetchProductsList é uma função', () => {
-    
+  it('fetchProductsList é uma função',  () => {
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+   await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled()
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
-
-  // it('...', () => {
-  // });
+  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
+    await fetchProductsList('computador');
+    expect(fetchProductsList('computador').toEqual(computadorSearch));
+  });
 });
