b03938b1bd6c64f1a3b866524b8693d2ae52c53a
Daniel <daniel.gomes.9@hotmail.com>
2023-02-15 16:34:16 -0300

    resolve requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const produtsElement = document.querySelector('.products');
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const getProduts = await fetchProductsList('computador');
+
+Object.values(getProduts).forEach((product) => {
+  const iten = createProductElement(product);
+  produtsElement.appendChild(iten);
+});
